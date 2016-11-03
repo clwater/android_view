ef8612ad1dec513422df005eb0fe8fc839035a1b
gengzhibo <gengzhibo@gengzhibo.net>
2016-11-03 14:54:17 +0800

    20161103

--- AndroidView/app/src/main/java/clwater/androidview/MainActivity.java
+++ AndroidView/app/src/main/java/clwater/androidview/MainActivity.java
@@ -2,12 +2,43 @@ package clwater.androidview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import clwater.androidview.view.TextButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
+
+    private LinearLayout linearLayout;
+    private TextButton testbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearLayout = (LinearLayout) findViewById(R.id.mylayout);
+        testbtn = (TextButton) findViewById(R.id.test_mybtn);
+
+        linearLayout.setOnTouchListener(this);
+        testbtn.setOnTouchListener(this);
+
+        linearLayout.setOnClickListener(this);
+        testbtn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        Log.i("view", "OnTouchListener--onTouch-- action="+event.getAction()+" --"+v);
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.i("view", "OnClickListener--onClick--"+v);
     }
 }
---
+++ AndroidView/app/src/main/java/clwater/androidview/view/TextButton.java
@@ -0,0 +1,29 @@
+package clwater.androidview.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Button;
+
+/**
+ * Created by gengzhibo on 16/11/2.
+ */
+
+public class TextButton extends Button {
+    public TextButton(Context context , AttributeSet attre) {
+        super(context , attre);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        Log.i("view", "dispatchTouchEvent-- action=" + event.getAction());
+        return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.i("view", "onTouchEvent-- action="+event.getAction());
+        return false;
+    }
+}
